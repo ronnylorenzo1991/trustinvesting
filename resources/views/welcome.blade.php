e2c8a6a07c7248f0c37ef81b49ee4dd11afaef62
Ronny Garcia Lorenzo <garcialorenzo1991@yahoo.com>
2021-03-21 15:19:45 -0400

    fix color for fast begin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -734,7 +734,7 @@
     </section>
 
     {{-- Begin fast --}}
-    <section class="pb-20 relative block">
+    <section class="pb-20 relative block bg-blue-500">
         <div
             class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
             style="height: 80px; transform: translateZ(0px);"
