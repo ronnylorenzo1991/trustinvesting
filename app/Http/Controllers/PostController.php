a9a698bfcbc5a5a97d82af824693e49279fb06da
Ronny Garcia Lorenzo <garcialorenzo1991@yahoo.com>
2021-03-24 19:56:13 -0400

    Added meta tags to posts and refactor post show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,25 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Artesaos\SEOTools\Facades\OpenGraph;
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index ()
     {
-        $posts = Post::where('status', 2)->latest('id')->paginate(10);
+        SEOTools  ::setTitle ('Blog');
+        SEOTools  ::setDescription ('En este blog usted obtendrá lo mas novedoso en temas relacionados con inversiones en criptoactivos');
+        OpenGraph ::setDescription ('En este blog usted obtendrá lo mas novedoso en temas relacionados con inversiones en criptoactivos');
+        OpenGraph ::setTitle ('Blog');
+        OpenGraph ::setUrl ('https://www.trustinvesting.us/blog');
+        OpenGraph ::addProperty ('type', 'articles');
 
-        return view('post.index', compact('posts'));
+        $posts = Post ::where ('status', 2) -> latest ('id') -> paginate (10);
+
+        return view ('post.index', compact ('posts'));
     }
 
     public function show(Post $post)
     {
-        $related = Post::where('category_id', $post->category_id)
-            ->where('status', 2)
-            ->where('id','!=', $post->id)
-            ->latest('id')
-            ->get();
+        SEOTools  ::setTitle ($post->name);
+        SEOTools  ::setDescription ($post->extract);
+        OpenGraph ::setDescription ($post->extract);
+        OpenGraph ::setTitle ($post->name);
+        OpenGraph ::setUrl ('https://www.trustinvesting.us/' . $post->slug);
+        OpenGraph ::addProperty ('type', 'articles');
 
-        return view('post.show', compact('post', 'related'));
+        $related = Post ::where ('category_id', $post -> category_id)
+            -> where ('status', 2)
+            -> where ('id', '!=', $post -> id)
+            -> latest ('id')
+            -> get ();
+
+        return view ('post.show', compact ('post', 'related'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/manage-plans', [HomeController::class, 'manage'])
 
 Route::get('/blog', [PostController::class, 'index'])->name('post.index');
 
-Route::get('post/{post}/show', [PostController::class, 'show'])->name('post.show');
+Route::get('post/{post}', [PostController::class, 'show'])->name('post.show');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
